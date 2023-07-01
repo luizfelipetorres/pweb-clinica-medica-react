c47224f9e8b91a17cbe231bacb2214bd6d674c35
Luis Fernando Cezar Dos Santos <nandocezar22@gmail.com>
2023-07-01 14:02:38 -0300

    feat: implments medic form and refactor code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import MedicForm from './view/forms/medic/MedicForm';
 
 export default () => {
   return (
     <React.Fragment className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MedicForm />
     </React.Fragment>
   );
 }
---
+++ src/view/forms/medic/MedicForm.jsx
@@ -0,0 +1,149 @@
+import { useForm } from "react-hook-form";
+import { isEmail } from "validator";
+import AddressForm from "../general/AddressForm";
+import '../form.css';
+
+
+export default () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
+  return (
+    <div className="app-container">
+
+      <div className="form-group">
+        <h2>Profissional</h2>
+      </div>
+
+      <div className="form-group">
+        <label>Nome completo</label>
+        <input
+          className={errors?.name && "input-error"}
+          type="text"
+          placeholder="Seu nome completo"
+          {...register("name", { required: true })}
+        />
+        {errors?.name?.type === "required" && (
+          <p className="error-message">Nome completo é obrigatório.</p>
+        )}
+      </div>
+
+      <div>
+        <div className="form-group">
+          <label>Especialidade</label>
+          <select
+            className={errors?.specialty && "input-error"}
+            defaultValue="0"
+            {...register("specialty", { validate: (value) => value !== "0" })}
+          >
+            <option value="0">Selecione uma especialidade...</option>
+            <option value="ortopedia">Ortopedia</option>
+            <option value="cardiologia">Cardiologia</option>
+            <option value="ginecologia">Ginecologia</option>
+            <option value="dermatologia">Dermatologia</option>
+          </select>
+
+          {errors?.specialty?.type === "validate" && (
+            <p className="error-message">Especialidade é obrigatório.</p>
+          )}
+        </div>
+
+        <div className="form-group">
+          <label>CRM</label>
+          <input
+            className={errors?.name && "input-error"}
+            type="text"
+            placeholder="Seu CRM"
+            {...register("crm", { required: true })}
+          />
+          {errors?.CRM?.type === "required" && (
+            <p className="error-message">CRM é obrigatório.</p>
+          )}
+        </div>
+      </div>
+      
+      <div className="form-group">
+        <h2>Contato</h2>
+      </div>
+
+      <div className="form-group">
+        <label>E-mail</label>
+        <input
+          className={errors?.email && "input-error"}
+          type="email"
+          placeholder="Seu e-mail"
+          {...register("email", {
+            required: true,
+            validate: (value) => isEmail(value),
+          })}
+        />
+        {errors?.email?.type === "required" && (
+          <p className="error-message">Email é obrigatório.</p>
+        )}
+
+        {errors?.email?.type === "validate" && (
+          <p className="error-message">Email é válido.</p>
+        )}
+      </div>
+
+      <div className="form-group">
+        <label>Telefone</label>
+        <input
+          className={errors?.email && "input-error"}
+          type="email"
+          placeholder="Seu telefone"
+          {...register("telephone", {
+            required: true
+          })}
+        />
+        {errors?.telephone?.type === "required" && (
+          <p className="error-message">Telefone é obrigatório.</p>
+        )}
+      </div>
+
+      <div className="form-group">
+        <h2>Endereço</h2>
+      </div>
+
+      <AddressForm register={register} errors={errors}/>
+
+      <div className="form-group">
+        <h2>Termo de compromisso</h2>
+      </div>
+
+      <div className="form-group">
+        <div className="checkbox-group">
+          <input
+            type="checkbox"
+            name="privacy-policy"
+            {...register("privacyTerms", {
+              validate: (value) => value === true,
+            })}
+          />
+          <label>Confirmo que as informações fornecidas estão corretas.</label>
+        </div>
+
+        {errors?.privacyTerms?.type === "validate" && (
+          <p className="error-message">
+            Você deve confirmar que as informações fornecidas estão corretas.
+          </p>
+        )}
+      </div>
+
+      <div className="form-group">
+        <button onClick={() => handleSubmit(onSubmit)()}>Salvar</button>
+      </div>
+      
+      <div className="form-group">
+        <button onClick={() => alert('Cancelado')}>Cancelar</button>
+      </div>
+    </div>
+  );
+};
