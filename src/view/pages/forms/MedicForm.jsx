9826065411fef2f5a8ea9e1fcf00cf5cae91b812
Luis Fernando Cezar Dos Santos <nandocezar22@gmail.com>
2023-07-02 16:25:02 -0300

    feat: implement post and refactor code

--- src/view/pages/forms/MedicForm.jsx
+++ src/view/pages/forms/MedicForm.jsx
@@ -1,86 +1,160 @@
+import * as React from 'react';
 import AddIcon from "@mui/icons-material/Add";
 import CleaningIcon from "@mui/icons-material/CleaningServices";
-import { Box, Fab } from "@mui/material";
+import { Fab } from "@mui/material";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Divider from '@mui/material/Divider';
+import { pink } from '@mui/material/colors';
+import Checkbox from '@mui/material/Checkbox';
 import { useForm } from "react-hook-form";
-import "./form.css";
 import AddressForm from "./general/AddressForm";
 import PersonalDataForm from "./general/PersonalDataForm";
+import Api from "../../../services/Api";
+import { toast } from "react-toastify";
+import "./form.css";
 
-export default () => {
+
+export default ({ isUpdated }) => {
+
+  
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
+  const onSubmit = async (data) => {
+    await Api.post("/medico", {
+      nome: data.nome,
+      email: data.email,
+      crm: data.crm,
+      especialidade: data.especialidade,
+      telefone: data.telefone,
+      endereco: {
+        logradouro: data.logradouro,
+        bairro: data.bairro,
+        cep: data.cep,
+        cidade: data.cidade,
+        uf: data.uf,
+        numero: data.numero,
+        complemento: data.complemento
+      }
+    })
+      .then(function (response) {
+        toast.success('Cadastro de ' + response.data.nome + ' realizado com sucesso!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      })
+      .catch(function (error) {
+        toast.error(error, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      });
   };
 
   return (
     <>
       <div className="app-container">
-        <PersonalDataForm register={register} errors={errors} />
+
+        <div className="form-group">
+          <PersonalDataForm register={register} errors={errors} />
+        </div>
+
 
         <div className="form-group">
           <h2>Profissional</h2>
-          <label>Especialidade</label>
-          <select
-            className={errors?.specialty && "input-error"}
-            defaultValue="0"
-            {...register("specialty", { validate: (value) => value !== "0" })}
-          >
-            <option value="0">Selecione uma especialidade...</option>
-            <option value="ortopedia">Ortopedia</option>
-            <option value="cardiologia">Cardiologia</option>
-            <option value="ginecologia">Ginecologia</option>
-            <option value="dermatologia">Dermatologia</option>
-          </select>
-
-          {errors?.specialty?.type === "validate" && (
+          <Divider />
+          <Box marginTop={2} sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Especialidade</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                variant="outlined"
+                className={errors?.especialidade && "input-error"}
+                defaultValue="0"
+                {...register("especialidade", { validate: (value) => value !== "0" })}
+              >
+                <MenuItem value={"0"}>Selecione...</MenuItem>
+                <MenuItem value={"ORTOPEDIA"}>ORTOPEDIA</MenuItem>
+                <MenuItem value={"CARDIOLOGIA"}>CARDIOLOGIA</MenuItem>
+                <MenuItem value={"GINECOLOGIA"}>GINECOLOGIA</MenuItem>
+                <MenuItem value={"DERMATOLOGIA"}>DERMATOLOGIA</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          {errors?.especialidade?.type === "validate" && (
             <p className="error-message">Especialidade é obrigatório.</p>
           )}
 
-          <label>CRM</label>
-          <input
-            className={errors?.name && "input-error"}
-            type="text"
-            placeholder="Seu CRM"
-            {...register("crm", { required: true })}
-          />
-          {errors?.CRM?.type === "required" && (
+          <Box marginTop={2}>
+            <FormControl fullWidth>
+              <TextField
+                id="outlined-basic"
+                label="CRM"
+                variant="outlined"
+                className={errors?.crm && "input-error"}
+                type="text"
+                placeholder="Digite seu CRM..."
+                {...register("crm", { required: true })} />
+            </FormControl>
+          </Box>
+          {errors?.crm?.type === "required" && (
             <p className="error-message">CRM é obrigatório.</p>
           )}
         </div>
 
-        <AddressForm register={register} errors={errors} />
+        <div className="form-group">
+          <AddressForm register={register} errors={errors} />
+        </div>
 
         <div className="form-group">
           <h2>Termo de compromisso</h2>
-
-          <div className="checkbox-group">
-            <input
-              type="checkbox"
-              name="privacy-policy"
+          <div className='checkbox-group'>
+            <Checkbox
+              defaultChecked
+              sx={{
+                color: pink[800],
+                '&.Mui-checked': {
+                  color: pink[600],
+                },
+              }}
               {...register("privacyTerms", {
                 validate: (value) => value === true,
               })}
             />
-            <label>
-              Confirmo que as informações fornecidas estão corretas.
-            </label>
+            <label>Confirmo que as informações fornecidas estão corretas.</label>
           </div>
-
           {errors?.privacyTerms?.type === "validate" && (
             <p className="error-message">
               Você deve confirmar que as informações fornecidas estão corretas.
             </p>
           )}
-          <Fab color="primary" variant="extended">
+          
+          <Fab color="primary" variant="extended" onClick={() => handleSubmit(onSubmit)()}>
             <AddIcon sx={{ mr: 1 }} />
-            Cadastrar novo médico
+            Salvar
           </Fab>
-
           <Fab color="primary" variant="extended">
             <CleaningIcon sx={{ mr: 1 }} />
             Limpar campos
--- src/view/pages/forms/general/PersonalDataForm.jsx
+++ src/view/pages/forms/general/PersonalDataForm.jsx
@@ -1,60 +1,76 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Divider from '@mui/material/Divider';
 import { isEmail, isMobilePhone } from "validator";
 
 import "../form.css";
 
-export default ({ register, errors }) => {
+export default ({ register, errors, isUpdate }) => {
   return (
     <>
-      <div className="form-group">
-        <h2>Dados pessoais</h2>
-        <label>Nome completo</label>
-        <input
-          className={errors?.name && "input-error"}
-          type="text"
-          placeholder="Seu nome completo"
-          {...register("name", { required: true })}
-        />
-        {errors?.name?.type === "required" && (
-          <p className="error-message">Nome completo é obrigatório.</p>
-        )}
+      <h2>Dados pessoais</h2>
+      <Divider />
+      <Box marginTop={2}>
+        <FormControl fullWidth>
+          <TextField
+            id="outlined-basic"
+            label="Nome completo"
+            variant="outlined"
+            className={errors?.nome && "input-error"}
+            type="text"
+            placeholder="Digite seu nome completo..."
+            {...register("nome", { required: true })} />
+        </FormControl>
+      </Box>
+      {errors?.nome?.type === "required" && (
+        <p className="error-message">Nome completo é obrigatório.</p>
+      )}
 
-        <label>E-mail</label>
-        <input
-          className={errors?.email && "input-error"}
-          type="email"
-          placeholder="Seu e-mail"
-          {...register("email", {
-            required: true,
-            validate: (value) => isEmail(value),
-          })}
-        />
-        {errors?.email?.type === "required" && (
-          <p className="error-message">Email é obrigatório.</p>
-        )}
+      <Box marginTop={2}>
+        <FormControl fullWidth>
+          <TextField
+            id="outlined-basic"
+            label="E-mail"
+            variant="outlined"
+            className={errors?.email && "input-error"}
+            type="text"
+            placeholder="Digite seu e-mail..."
+            {...register("email", {
+              required: true,
+              validate: (value) => isEmail(value),
+            })} />
+        </FormControl>
+      </Box>
+      {errors?.email?.type === "required" && (
+        <p className="error-message">Email é obrigatório.</p>
+      )}
+      {errors?.email?.type === "validate" && (
+        <p className="error-message">Email inválido.</p>
+      )}
 
-        {errors?.email?.type === "validate" && (
-          <p className="error-message">Email inválido.</p>
-        )}
-
-        <label>Telefone</label>
-        <input
-          className={errors?.email && "input-error"}
-          type="tel"
-          placeholder="Seu telefone"
-          {...register("telephone", {
-            required: true,
-            validate: (value) => isMobilePhone(value, 'pt-BR')
-          })}
-        />
-        {errors?.telephone?.type === "required" && (
-          <p className="error-message">Telefone é obrigatório.</p>
-        )}
-        {errors?.telephone?.type === "validate" && (
-          <p className="error-message">Telefone inválido</p>
-        )}
-        
-      </div>
+      <Box marginTop={2}>
+        <FormControl fullWidth>
+          <TextField
+            id="outlined-basic"
+            label="Telefone"
+            variant="outlined"
+            className={errors?.telefone && "input-error"}
+            type="text"
+            placeholder="Digite seu telefone..."
+            {...register("telefone", {
+              required: true,
+              validate: (value) => isMobilePhone(value, 'pt-BR')
+            })} />
+        </FormControl>
+      </Box>
+      {errors?.telefone?.type === "required" && (
+        <p className="error-message">Telefone é obrigatório.</p>
+      )}
+      {errors?.telefone?.type === "validate" && (
+        <p className="error-message">Telefone inválido</p>
+      )}
     </>
   );
 };
