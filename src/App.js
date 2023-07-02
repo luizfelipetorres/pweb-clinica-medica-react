9bef118bc6d49d09ae85d1012fa9815b1b91de16
Luis Fernando Cezar Dos Santos <nandocezar22@gmail.com>
2023-07-02 00:54:23 -0300

    feat: create form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from 'react';
-import MedicForm from './view/forms/medic/MedicForm';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import MedicManager from './view/manager/medic/MedicManager';
 
 export default () => {
   return (
     <React.Fragment className="App">
       <ToastContainer />
-      <MedicForm />
+      <MedicManager />
     </React.Fragment>
   );
 }
